c642b9d22ab8eee11ccaec0eee975d3340696579
Phuc Tran <glup3.tran@gmail.com>
2019-11-20 14:14:05 +0100

    fix async

--- src/index.js
+++ src/index.js
@@ -32,54 +32,54 @@ app.post(v1Prefix + '/init-defaults', (req, res) => {
   Tip.destroy({
     where: {},
     truncate: true
+  }).then(() => {
+    Tip.bulkCreate([
+      {
+        author: 'Glup3',
+        title: 'Tipp Numero Uno',
+        description: 'hier kommt einfach irgendein geiler text rein',
+        schoolClass: '5CHIF',
+        vintage: 2019,
+        department: 'Informatik',
+        issueDate: new Date(),
+        verified: true,
+        gender: 'm'
+      },
+      {
+        author: null,
+        title: 'Treppen',
+        description: 'Immer eine Stufe überspringen - spart Kraft',
+        schoolClass: '5CHIF',
+        vintage: 2019,
+        department: 'Informatik',
+        issueDate: new Date(),
+        verified: false,
+        gender: 'w'
+      },
+      {
+        author: null,
+        title: 'Anonym',
+        description: 'Ein Secret',
+        schoolClass: null,
+        vintage: 2019,
+        department: null,
+        issueDate: new Date(),
+        verified: true,
+        gender: 'm'
+      },
+      {
+        author: 'Max',
+        title: 'DSGVO',
+        description: 'Jeder muss es machen, macht keinen Spaß',
+        schoolClass: '5CHIF',
+        vintage: 2019,
+        department: 'Biomedizin',
+        issueDate: new Date(),
+        verified: false,
+        gender: null
+      }
+    ]);
   });
-
-  Tip.bulkCreate([
-    {
-      author: 'Glup3',
-      title: 'Tipp Numero Uno',
-      description: 'hier kommt einfach irgendein geiler text rein',
-      schoolClass: '5CHIF',
-      vintage: 2019,
-      department: 'Informatik',
-      issueDate: new Date(),
-      verified: true,
-      gender: 'm'
-    },
-    {
-      author: null,
-      title: 'Treppen',
-      description: 'Immer eine Stufe überspringen - spart Kraft',
-      schoolClass: '5CHIF',
-      vintage: 2019,
-      department: 'Informatik',
-      issueDate: new Date(),
-      verified: false,
-      gender: 'w'
-    },
-    {
-      author: null,
-      title: 'Anonym',
-      description: 'Ein Secret',
-      schoolClass: null,
-      vintage: 2019,
-      department: null,
-      issueDate: new Date(),
-      verified: true,
-      gender: 'm'
-    },
-    {
-      author: 'Max',
-      title: 'DSGVO',
-      description: 'Jeder muss es machen, macht keinen Spaß',
-      schoolClass: '5CHIF',
-      vintage: 2019,
-      department: 'Biomedizin',
-      issueDate: new Date(),
-      verified: false,
-      gender: null
-    }
-  ]);
 
   res.json('Success');
 });
